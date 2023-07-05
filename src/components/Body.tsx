6dda1237caa01df9a9600c61f9994231bde5ab5c
David Costa <davidamorimc@gmail.com>
2023-07-05 22:38:44 +0100

    Initial commit

---
+++ src/components/Body.tsx
@@ -0,0 +1,18 @@
+// React component with two input values
+
+import { useState } from "react";
+import { Grid, Typography } from "@mui/material";
+
+function Body() {
+  const [something] = useState("Hello World!!");
+
+  return (
+    <Grid container direction={"column"} className="main-paper">
+      <Grid item xs={12}>
+        <Typography>{something}</Typography>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default Body;
